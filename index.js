b4d16fd137aabc18cff3190641d4556d1ace6413
Divyanshi Rastogi <43536638+Divya-5@users.noreply.github.com>
2022-01-30 17:03:51 +0530

    combinereducers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require('redux');
 const createStore=redux.createStore;
+const combineReducers= redux.combineReducers;
 const BUY_EGG = 'BUY_EGG'
 const BUY_CHICKEN = 'BUY_CHICKEN'
 //action creators
@@ -15,27 +16,39 @@ function buyChicken(){
         info:'Buying Chicken'
     }
 }
-const initialState={
-    numOfEggs:100,
+const initialEggState={
+    numOfEggs:100
+}
+const initialChickenState={
     numOfChickens:20
 }
 
-const reducer = (state=initialState, action) => {
+const eggReducer = (state=initialEggState, action) => {
 switch(action.type){
     case BUY_EGG: return{//object destructing 
         ...state,//spread operator
         numOfEggs:state.numOfEggs-1
     }
-    case BUY_CHICKEN: return{
-        ...state,
-        numOfChickens:state.numOfChickens-1
-    }
   default: return state;
 }
-
 }
 
-const store=createStore(reducer);
+const chickenReducer = (state=initialChickenState, action) => {
+    switch(action.type){
+        case BUY_CHICKEN: return{
+            ...state,
+            numOfChickens:state.numOfChickens-1
+        }
+      default: return state;
+    }
+    
+    }
+
+    const rootReducer=combineReducers({
+        egg:eggReducer,
+        chicken:chickenReducer
+    })
+const store=createStore(rootReducer);
 console.log('Initial State', store.getState());
 const unsubscribe = store.subscribe(()=> console.log('Updated State', store.getState()));
 store.dispatch(buyEgg());
